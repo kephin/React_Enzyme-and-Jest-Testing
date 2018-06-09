b7755840833fd07118af0e62611bb8a40d8a5145
kephin <wunshon76325@gmail.com>
2018-06-09 17:34:54 +0800

    test: CommentList insilde App

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -3,8 +3,14 @@ import { shallow } from 'enzyme';
 
 import App from '../App';
 import CommentBox from '../CommentBox';
+import CommentList from '../CommentList';
 
 it('shows a comment box', () => {
   const wrapper = shallow(<App />);
   expect(wrapper.find(CommentBox).length).toEqual(1);
 });
+
+it('shows a comment list', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find(CommentList).length).toEqual(1);
+})
