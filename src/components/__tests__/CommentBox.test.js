50d7d9cf8442c02d03ad4fa7bebaf4117c3e8ea9
kephin <wunshon76325@gmail.com>
2018-06-18 12:19:38 +0800

    test: mock saveComment fucntion for testing submit

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import CommentBox from 'components/CommentBox';
+import { CommentBox } from 'components/CommentBox';
 
 describe('<CommentBox />', () => {
-  const wrapper = shallow(<CommentBox />);
+  const mockSaveComment = jest.fn();
+  const wrapper = shallow(<CommentBox saveComment={mockSaveComment} />);
+
   it('shows a textarea and a button', () => {
     expect(wrapper.find('textarea').length).toEqual(1);
     expect(wrapper.find('button').length).toEqual(1);
   });
 
-  it('can enter input into the textarea', () => {
+  it('can enter input into the textarea and submit', () => {
     wrapper.find('textarea').simulate('change', { target: { value: 'new comment' } });
     wrapper.update();
     expect(wrapper.find('textarea').prop('value')).toEqual('new comment');
+    wrapper.find('form').simulate('submit', { preventDefault() { } });
+    expect(mockSaveComment.mock.calls[0][0]).toEqual('new comment');
+    expect(mockSaveComment.mock.calls.length).toEqual(1);
   });
 
   it('when the input is submitted, textarea should be emptied', () => {
-    wrapper.find('form').simulate('submit', { preventDefault() {} });
-    wrapper.update();
     expect(wrapper.find('textarea').prop('value')).toEqual('');
   });
 });
