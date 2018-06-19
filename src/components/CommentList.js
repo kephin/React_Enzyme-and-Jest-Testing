b41f853393c7ee6efff677c2dc75ae9b2b0286d3
kephin <wunshon76325@gmail.com>
2018-06-20 00:10:46 +0800

    feat: add CommentList component

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,5 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default () => (
-  <div>Comment List</div>
-);
+export class CommentList extends Component {
+  renderComments() {
+    return this.props.comments.map(comment => <li key={comment}>{comment}</li>);
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.renderComments()}
+      </ul>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  comments: state.comments,
+});
+
+export default connect(mapStateToProps)(CommentList);
