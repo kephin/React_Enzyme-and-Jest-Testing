3321e1dd16cdaa70a0e3bc737c7ee5e9d7deb352
kephin <wunshon76325@gmail.com>
2018-07-01 17:16:49 +0800

    feat: create HOC scaffold file

---
+++ src/components/requireAuth.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default ChildComponent => class ComposedComponent extends Component {
+  render() {
+    return (
+      <ChildComponent />
+    );
+  }
+};
